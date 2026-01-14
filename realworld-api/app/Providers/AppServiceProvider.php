aa8978fdb16cc03df90362037610c77c8d8dd02c
Tomislav Vinkovic <tomislav>
2026-01-14 15:16:41 +0100

    Authorization header updated to 'Token' instead of 'Bearer'

--- realworld-api/app/Providers/AppServiceProvider.php
+++ realworld-api/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Tymon\JWTAuth\Http\Parser\QueryString;
+use Tymon\JWTAuth\Http\Parser\AuthHeaders;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Http\Parser\InputSource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 1. Create a new AuthHeaders parser
+        $headerParser = new AuthHeaders();
+        
+        // 2. Set the prefix to "Token" (instead of the default "Bearer")
+        $headerParser->setHeaderPrefix('Token');
+
+        // 3. Set the chain of parsers on the JWTAuth instance
+        // This tells JWTAuth to use your custom header parser first
+        JWTAuth::parser()->setChain([
+            $headerParser,
+            new QueryString(),
+            new InputSource()
+        ]);
     }
 }
