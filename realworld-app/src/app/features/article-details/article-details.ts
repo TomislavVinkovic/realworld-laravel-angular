6ac45b10a6f589834d073c3a387bf1575458b7ee
Tomislav Vinkovic <tomislav>
2026-01-30 13:17:54 +0100

    added buttons to article details

--- realworld-app/src/app/features/article-details/article-details.ts
+++ realworld-app/src/app/features/article-details/article-details.ts
@@ -1,14 +1,15 @@
 import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { ArticleService } from '../../core/services/article-service';
 import { Article } from '../../core/models/article';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../core/auth/auth-service';
 import { CommentList } from "../../shared/ui/comment-list/comment-list";
+import { ProfileService } from '../../core/services/profile-service';
 
 @Component({
   selector: 'app-article-details',
-  imports: [DatePipe, RouterLink, CommentList],
+  imports: [DatePipe, RouterLink, CommentList, NgClass],
   templateUrl: './article-details.html',
   styleUrl: './article-details.css',
 })
@@ -16,18 +17,19 @@ export class ArticleDetails implements OnInit {
   readonly slug = input.required<string>();
   private readonly articleService = inject(ArticleService);
   private readonly authService = inject(AuthService);
+  private readonly profileService = inject(ProfileService);
   
   private readonly router = inject(Router);
 
   readonly currentUser = this.authService.currentUser;
 
   isLoading = signal(true);;
-  article: Article|null = null;
+  article = signal<Article|null>(null);
 
   ngOnInit(): void {
     this.articleService.getArticle(this.slug()).subscribe({
       next: (article: Article) => {
-        this.article = article;
+        this.article.set(article);
         this.isLoading.set(false);
       },
       error: (err: any) => {
@@ -39,14 +41,14 @@ export class ArticleDetails implements OnInit {
 
   editArticle() {
     // Navigate to the editor with the slug parameter
-    this.router.navigate(['/editor', this.article!.slug]);
+    this.router.navigate(['/editor', this.article()!.slug]);
   }
 
   deleteArticle() {
     const confirmed = window.confirm('Are you sure you want to delete this article?');
 
     if (confirmed) {
-      this.articleService.deleteArticle(this.article!.slug!).subscribe({
+      this.articleService.deleteArticle(this.article()!.slug!).subscribe({
         next: () => {
           this.router.navigate(['/']);
         },
@@ -56,5 +58,52 @@ export class ArticleDetails implements OnInit {
         }
       });
     }
+  }
+
+  toggleFollow() {
+    const p = this.article()?.author;
+    if (!p) return;
+
+    // Optimistic Update
+    this.article.update(current => current ? { 
+      ...current, 
+      author: { 
+        ...current.author!, 
+        following: !current.author!.following 
+      } 
+    } : null);
+
+    const request$ = p.following 
+      ? this.profileService.unfollow(p.username)
+      : this.profileService.follow(p.username);
+
+    request$.subscribe({
+      error: () => {
+        this.article.update(current => current ? { 
+          ...current, 
+          author: { 
+            ...current.author!, 
+            following: !current.author!.following 
+          } 
+        } : null);
+      }
+    });
+  }
+
+  toggleFavorite() {
+    const isFavorited = this.article()!.favorited;
+    const newCount = isFavorited ? this.article()!.favoritesCount! - 1 : this.article()!.favoritesCount! + 1;
+
+    const request$ = isFavorited
+      ? this.articleService.unfavorite(this.article()!.slug!)
+      : this.articleService.favorite(this.article()!.slug!);
+
+    this.article.update(a => a ? { ...a, favorited: !isFavorited, favoritesCount: newCount } : null);
+
+    request$.subscribe({
+      error: (err) => {
+        this.article.update(a => a ? { ...a, favorited: isFavorited, favoritesCount: newCount } : null);
+      }
+    });
   }
 }
